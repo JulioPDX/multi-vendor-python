3455d1eab0d772fb409177a789b4a97df98f9a2d
JulioPDX <juliop503@gmail.com>
2021-04-02 20:55:38 -0700

    Pushing up stuffs

---
+++ ospf_netmiko.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+
+# Import requirements
+from yaml import safe_load
+from netmiko import Netmiko
+from jinja2 import Environment, FileSystemLoader
+from netaddr import IPNetwork
+from rich import print as pr
+
+# functions to be used in jinja templates for IP management
+def address(a):
+    a = str(IPNetwork(a).ip)
+    return a
+
+
+def mask(b):
+    b = str(IPNetwork(b).netmask)
+    return b
+
+
+def main():
+
+    # Open hosts file as variable for future use
+    with open("hosts.yaml", "r") as handle:
+        host_root = safe_load(handle)
+    pr(host_root)
+
+    # Set platform map to match netmiko
+    platform_map = {"ios": "cisco_ios", "arista": "arista_eos", "aruba": "hp_procurve"}
+
+    # Assigning platform variable to each host
+    for host in host_root["host_list"]:
+        platform = platform_map[host["platform"]]
+
+        # Load in the host specific vars
+        with open(f"vars/{host['name']}.yaml", "r") as handle:
+            ospf = safe_load(handle)
+
+        # This portion is essentially configuring our jinja environment
+        j2_env = Environment(
+            loader=FileSystemLoader("."), trim_blocks=True, autoescape=True
+        )
+        # https://www.kite.com/python/answers/how-to-call-a-function-in-a-jinja2-template-in-python
+        j2_env.globals["address"] = address
+        j2_env.globals["mask"] = mask
+
+        template = j2_env.get_template(f"templates/netmiko/{platform}.j2")
+        new_ospf_config = template.render(data=ospf)
+        pr(f"\n{new_ospf_config}")
+
+        conn = Netmiko(
+            host=host["mgmt"],
+            username=host["username"],
+            password=host["password"],
+            device_type=platform,
+        )
+
+        pr(f"\n#### Logged into {conn.find_prompt()}, woohoo! ####")
+
+        result = conn.send_config_set(new_ospf_config.split("\n"))
+
+        print(result)
+
+        with open(f"backups/{host['name']}.conf", "w") as writer:
+            result = conn.send_command("show run")
+            writer.writelines(result)
+
+        conn.disconnect()
+
+
+if __name__ == "__main__":
+    main()
