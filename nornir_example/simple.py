993e47a3ec3734575919da0f609803d8e3c99ee6
JulioPDX <juliop503@gmail.com>
2021-04-03 14:51:04 -0700

    Starting Nornir portion of course. Created requirements for nornir 3.x

---
+++ nornir_example/simple.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Source from Nick Russo Course
+on Pluralsight
+"""
+
+from nornir import InitNornir
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir_utils.plugins.functions import print_result
+from rich import print
+import urllib3
+
+# Disable warnings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def main():
+
+    nornir = InitNornir(config_file="config.yaml")
+    result = nornir.run(task=napalm_get, getters=["get_facts"])
+
+    print_result(result)
+
+
+if __name__ == "__main__":
+    main()
